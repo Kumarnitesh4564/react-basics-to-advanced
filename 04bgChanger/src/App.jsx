ae9d124e28f37de19510e3131b7d800d619c3426
Nitesh Kumar <kumarnitesh70040@gmail.com>
2026-03-15 19:19:19 +0530

    Added React password generator project

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -3,7 +3,6 @@ import './App.css'
 
 function App() {
   const [color, setColor] = useState("olive");
-
   return (
     <div className='w-full h-screen duration-200'
     style={{backgroundColor: color}}>
